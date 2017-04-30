750747d638e74ab8939d9222e98d03501ea693ef
Artur Girenko <a.girenko@gmail.com>
2017-04-30 09:59:02 +0200

    call clear-subscription-cache! on code reload
    - as per changes of re-frame 0.9.0

--- resources/cljs-reagent6/main_dev.cljs
+++ resources/cljs-reagent6/main_dev.cljs
@@ -1,5 +1,6 @@
  (ns ^:figwheel-no-load env.$PLATFORM$.main
   (:require [reagent.core :as r]
+            [re-frame.core :refer [clear-subscription-cache!]]
             [$PROJECT_NAME_HYPHENATED$.$PLATFORM$.core :as core]
             [figwheel.client :as figwheel :include-macros true]))
 
@@ -9,9 +10,13 @@
 (defn reloader [] @cnt [core/app-root])
 (def root-el (r/as-element [reloader]))
 
+(defn force-reload! []
+  (clear-subscription-cache!)
+  (swap! cnt inc))
+
 (figwheel/watch-and-reload
  :websocket-url "ws://$DEV_HOST$:3449/figwheel-ws"
  :heads-up-display false
- :jsload-callback #(swap! cnt inc))
+ :jsload-callback force-reload!)
 
 (core/init)
